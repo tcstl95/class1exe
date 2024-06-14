57b3baa9681212f238ad0ac015310f9f60b6c0d6
Tom Carlson <tomcarlson171@gmail.com>
2024-06-13 20:02:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+ // Declare and assign the variables below
+ 
+// Use console.log to print the 'typeof' each variable. Print one item per line.
+
+console.log(typeof "17");
+console.log(typeof 3.2);
+console.log(typeof true)
+
+
+
+// Calculate a space mission below
+
+let missionKilometers = 34000000;
+let missionMiles = 21000000;
+let missionTravel= missionKilometers *missionMiles /23;
+
+
+
+
+// Print the results of the space mission calculations below
+console.log("It will take " + missionTravel + " years to complete the space mission");
+// Calculate a trip to the moon below
+let moonMiles= 238855;
+let moonKilometers= 384400;
+let moonTravel= moonMiles * moonKilometers / 23;
+// Print the results of the trip to the moon below
+console.log("It will take " + moonTravel + " years to complete the trip to the moon");
+
